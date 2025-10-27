e8d7043b7a1018aab87e9bf7d43596f44cd6a287
james <jdmichaelryan@gmail.com>
2025-10-27 17:55:34 +0000

    func: add listener object + thread

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from pynput import keyboard
-
+from keyboard_monitor import KeyboardMonitor
+import threading
+def run_listener():
+    with keyboard.Listener(on_press=KeyboardMonitor.on_press,on_release=KeyboardMonitor.on_release) as listener:
+        listener.join()
+# create the keyboard listener object
+if __name__ == '__main__':
+    thread1 = threading.Thread(target=run_listener)
+    thread1.start()
