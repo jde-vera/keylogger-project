1c8b627d542920dda077d1c2bd6c5a0be9aee1c8
james <jdmichaelryan@gmail.com>
2025-10-27 19:00:49 +0000

    fix: change func name

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pynput import keyboard
 from keyboard_monitor import KeyboardMonitor
 import threading
-def run_listener():
+def run_keyboard_listener():
     with keyboard.Listener(on_press=KeyboardMonitor.on_press,on_release=KeyboardMonitor.on_release) as listener:
         listener.join()
-# create the keyboard listener object
 if __name__ == '__main__':
-    thread1 = threading.Thread(target=run_listener)
+    thread1 = threading.Thread(target=run_keyboard_listener)
     thread1.start()
